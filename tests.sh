c2a5d77179ec85f23fdce779cc95da8eab68fa9e
Pavel Prischepa <pprischepa@gmail.com>
2016-12-20 14:05:46 +0700

    Updated tests

--- tests.sh
+++ tests.sh
@@ -27,18 +27,26 @@ fi
 
 echo 'Solr has started!'
 
+echo 'Create Solr cores'
+echo '========================================================================='
 docker exec "$name" solr-create-core core1
 docker exec "$name" solr-create-core core2 drupal7
 docker exec "$name" solr-create-core core3 drupal8
 
+echo 'Verify Solr cores'
+echo '========================================================================='
 docker exec "$name" curl -s http://localhost:8983/solr/core1/admin/ping
 docker exec "$name" curl -s http://localhost:8983/solr/core2/admin/ping
 docker exec "$name" curl -s http://localhost:8983/solr/core3/admin/ping
 
+echo 'Reload Solr cores'
+echo '========================================================================='
 docker exec "$name" solr-reload-core core1
 docker exec "$name" solr-reload-core core2
 docker exec "$name" solr-reload-core core3
 
+echo 'Delete Solr cores'
+echo '========================================================================='
 docker exec "$name" solr-delete-core core1
 docker exec "$name" solr-delete-core core2
 docker exec "$name" solr-delete-core core3
