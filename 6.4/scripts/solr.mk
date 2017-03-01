b56066a5c6422f40a34b00fa6d05e76a3e370e45
Chingis Sandanov <chingis@wodby.com>
2017-03-01 17:07:10 +0700

    Implement wait for solr command

--- 6.4/scripts/solr.mk
+++ 6.4/scripts/solr.mk
@@ -1,10 +1,6 @@
 -include rewrite.mk
 
-host ?= solr
-port ?= 8983
-config_set ?= data_driven_schema_configs
-
-.PHONY: create delete reload
+.PHONY: waitsolr create delete reload ping
 
 check_defined = \
     $(strip $(foreach 1,$1, \
@@ -15,16 +11,23 @@ __check_defined = \
 
 $(call check_defined, core, host, port)
 
+config_set ?= data_driven_schema_configs
+max_try ?= 12
+wait_seconds ?= 5
+
 default: create
 
-create:
+create: waitsolr
 	curl -sI "http://$(host):$(port)/solr/admin/cores?action=CREATE&name=$(core)&configSet=$(config_set)"
 
-delete:
+delete: waitsolr
 	curl -sI "http://$(host):$(port)/solr/admin/cores?action=UNLOAD&core=$(core)&deleteIndex=true&deleteDataDir=true&deleteInstanceDir=true"
 
-reload:
+reload: waitsolr
 	curl -sI "http://$(host):$(port)/solr/admin/cores?action=RELOAD&core=$(core)"
 
-ping:
+ping: waitsolr
 	curl -sI "http://$(host):$(port)/solr/$(core)/admin/ping"
+
+waitsolr:
+	/opt/docker-solr/scripts/wait-solr $(host) $(port) $(max_try) $(wait_seconds)
--- 6.4/test.sh
+++ 6.4/test.sh
@@ -6,42 +6,20 @@ if [[ ! -z $DEBUG ]]; then
   set -x
 fi
 
-host='solr'
-port='8983'
-
-waitForSolr() {
-    started=0
-
-    for i in {30..0}; do
-        if solr curl -s "http://${host}:${port}"; then
-            started=1
-            break
-        fi
-        echo 'Solr is starting...'
-        sleep 5
-    done
-
-    if [[ "$started" -eq '0' ]]; then
-        echo >&2 'Error. Solr is unreachable.'
-        exit 1
-    fi
-
-    echo 'Solr has started!'
-}
+HOST='solr'
+PORT='8983'
 
 solr() {
-    docker run --rm -i --link "${NAME}":"${host}" "${IMAGE}" "$@"
+    docker run --rm -i --link "${NAME}":"${HOST}" "${IMAGE}" "$@"
 }
 
-waitForSolr
-
 echo "Creating new core..."
-solr make core=core1 host=$host port=$port | grep "200 OK"
+solr make core=core1 host=$HOST port=$PORT | grep "200 OK"
 echo "Checking if core has been created..."
-solr make ping core=core1 host=$host port=$port | grep "200 OK"
+solr make ping core=core1 host=$HOST port=$PORT | grep "200 OK"
 echo "Reloading core..."
-solr make reload core=core1 host=$host port=$port | grep "200 OK"
+solr make reload core=core1 host=$HOST port=$PORT | grep "200 OK"
 echo "Deleting core..."
-solr make delete core=core1 host=$host port=$port | grep "200 OK"
+solr make delete core=core1 host=$HOST port=$PORT | grep "200 OK"
 echo "Checking if core has been deleted..."
-solr make ping core=core1 host=$host port=$port | grep "404 Not Found"
+solr make ping core=core1 host=$HOST port=$PORT | grep "404 Not Found"
