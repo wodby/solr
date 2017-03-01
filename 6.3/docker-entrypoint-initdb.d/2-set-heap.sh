fd7d2f0128e4a730886bbebe6cd994a0a5fcf7da
Chingis Sandanov <chingis@wodby.com>
2017-03-01 12:18:21 +0700

    Move init scripts to entrypoint

--- 6.3/docker-entrypoint-initdb.d/2-set-heap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if [[ ! -z $DEBUG ]]; then
-  set -x
-fi
-
-sed -i 's@^SOLR_HEAP=".*"@'"SOLR_HEAP=${SOLR_HEAP}"'@' /opt/solr/bin/solr.in.sh
--- 6.3/scripts/docker-entrypoint-solr.sh
+++ 6.3/scripts/docker-entrypoint-solr.sh
@@ -6,6 +6,18 @@ if [[ ! -z $DEBUG ]]; then
   set -x
 fi
 
+sudo /opt/docker-solr/scripts/solr-fix-permissions
+
+if [[ ! -e /opt/solr/server/solr/solr.xml ]]; then
+    cp /opt/docker-solr/solr.xml /opt/solr/server/solr/solr.xml
+fi
+
+if [[ ! -d /opt/solr/server/solr/configsets ]]; then
+    cp -r /opt/docker-solr/configsets /opt/solr/server/solr/configsets
+fi
+
+sed -i 's@^SOLR_HEAP=".*"@'"SOLR_HEAP=${SOLR_HEAP}"'@' /opt/solr/bin/solr.in.sh
+
 if [[ "${1}" == 'make' ]]; then
     exec "$@" -f /opt/docker-solr/scripts/solr.mk
 else
